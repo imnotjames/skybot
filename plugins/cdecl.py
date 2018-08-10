5ff53faa9f51203799d3d82f60019fbbcdd3193c
James Ward <james@notjam.es>
2018-08-10 10:52:32 -0400

    consistently use spaces throughout codebase
    per pep8

--- plugins/cdecl.py
+++ plugins/cdecl.py
@@ -3,5 +3,5 @@ from util import hook, http
 
 @hook.command
 def cdecl(inp):
-	'''.cdecl <expr> -- translate between C declarations and English, using cdecl.org'''
-	return http.get("http://cdecl.org/query.php", q=inp)
+    '''.cdecl <expr> -- translate between C declarations and English, using cdecl.org'''
+    return http.get("http://cdecl.org/query.php", q=inp)
