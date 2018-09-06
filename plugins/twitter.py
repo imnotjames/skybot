453375046aaf3559bdf4c446f0d1df3e5267c826
James Ward <james@notjam.es>
2018-09-06 00:36:24 -0400

    minor rewrite of twitter plugin to break it up
    refactor the twitter plugin to make it clearer how it works
    and to make it easier to test in unit tests

--- plugins/twitter.py
+++ plugins/twitter.py
@@ -1,9 +1,95 @@
-import random
+from random import choice
 import re
 from time import strptime, strftime
-from urllib import quote
 
 from util import hook, http
+
+
+TWITTER_URL_TIMELINE = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+TWITTER_URL_TWEETS = 'https://api.twitter.com/1.1/search/tweets.json'
+TWITTER_URL_STATUS = 'https://api.twitter.com/1.1/statuses/show.json'
+
+
+def twitter_request(api_key, url, params=None):
+    if not params:
+        params = {}
+
+    params['tweet_mode'] = 'extended'
+
+    try:
+        return http.get_json(url, oauth=True, oauth_keys=api_key, params=params)
+    except http.HTTPError as e:
+        errors = {
+            400: 'bad request (ratelimited?)',
+            401: 'unauthorized',
+            403: 'forbidden',
+            404: 'invalid user/id',
+            500: 'twitter is broken',
+            502: 'twitter is down ("getting upgraded")',
+            503: 'twitter is overloaded (lol, RoR)',
+            410: 'twitter shut off api v1.'
+        }
+
+        if e.code in errors:
+            raise ValueError('error: ' + errors[e.code])
+
+        raise ValueError('error: unknown %s' % e.code)
+
+
+def get_tweet_by_id(api_key, id):
+    return twitter_request(api_key, TWITTER_URL_TIMELINE, { 'id': id })
+
+
+def get_tweet_by_username(api_key, username, index=None):
+    response = twitter_request(api_key, TWITTER_URL_TIMELINE, {'screen_name': username})
+
+    if not 'statuses' in response:
+        return None
+
+    tweets = response["statuses"]
+
+    if index is None:
+        index = 0
+
+    if index not in tweets:
+        return None
+
+    return tweets[index]
+
+
+def get_tweet_by_search_term(api_key, search_term, index=None):
+    response = twitter_request(api_key, TWITTER_URL_TIMELINE, {'q': search_term})
+
+
+    if 'statuses' not in response:
+        return None
+
+    tweets = response["statuses"]
+
+    if index is None:
+        return choice(tweets)
+
+    if index not in tweets:
+        return None
+
+    return tweets[index]
+
+
+def format_tweet(tweet):
+    if "retweeted_status" in tweet:
+        rt = tweet["retweeted_status"]
+        rt_text = http.unescape(rt["full_text"]).replace('\n', ' ')
+        text = "RT @%s %s" % (rt["user"]["screen_name"], rt_text)
+    else:
+        text = http.unescape(tweet["full_text"]).replace('\n', ' ')
+
+    screen_name = tweet["user"]["screen_name"]
+    time = tweet["created_at"]
+
+    time = strptime(time, '%a %b %d %H:%M:%S +0000 %Y')
+    time = strftime("%Y-%m-%d %H:%M:%S", time)
+
+    return "%s \x02%s\x02: %s" % (time, screen_name, text)
 
 
 @hook.api_key('twitter')
@@ -12,79 +98,31 @@ def twitter(inp, api_key=None):
     ".twitter <user>/<user> <n>/<id>/#<search>/#<search> <n> -- " \
         "get <user>'s last/<n>th tweet/get tweet <id>/do <search>/get <n>th <search> result"
 
-    if not isinstance(api_key, dict) or any(key not in api_key for key in
-                                            ('consumer', 'consumer_secret', 'access', 'access_secret')):
-        return "error: api keys not set"
+    if not isinstance(api_key, dict):
+        return 'error: api keys not set'
 
-    getting_id = False
-    doing_search = False
-    index_specified = False
+    if any(key not in api_key for key in ('consumer', 'consumer_secret', 'access', 'access_secret')):
+        return 'error: api keys not set'
 
-    if re.match(r'^\d+$', inp):
-        getting_id = True
-        request_url = "https://api.twitter.com/1.1/statuses/show.json?id=%s" % inp
-    else:
-        try:
-            inp, index = re.split('\s+', inp, 1)
-            index = int(index)
-            index_specified = True
-        except ValueError:
-            index = 0
-        if index < 0:
-            index = 0
-        if index >= 20:
-            return 'error: only supports up to the 20th tweet'
-
-        if re.match(r'^#', inp):
-            doing_search = True
-            request_url = "https://api.twitter.com/1.1/search/tweets.json?q=%s" % quote(inp)
-        else:
-            request_url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=%s" % inp
+    if re.match(r"^\d+$", inp):
+        return format_tweet(get_tweet_by_id(inp))
 
     try:
-        tweet = http.get_json(request_url, oauth=True, oauth_keys=api_key, tweet_mode="extended")
-    except http.HTTPError, e:
-        errors = {400: 'bad request (ratelimited?)',
-                  401: 'unauthorized',
-                  403: 'forbidden',
-                  404: 'invalid user/id',
-                  500: 'twitter is broken',
-                  502: 'twitter is down ("getting upgraded")',
-                  503: 'twitter is overloaded (lol, RoR)',
-                  410: 'twitter shut off api v1.'}
-        if e.code == 404:
-            return 'error: invalid ' + ['username', 'tweet id'][getting_id]
-        if e.code in errors:
-            return 'error: ' + errors[e.code]
-        return 'error: unknown %s' % e.code
+        inp, index = re.split("\s+", inp, 1)
+        index = int(index)
 
-    if doing_search:
-        try:
-            tweet = tweet["statuses"]
-            if not index_specified:
-                index = random.randint(0, len(tweet) - 1)
-        except KeyError:
-            return 'error: no results'
+        if 0 > index >= 20:
+            index = 0
+    except ValueError:
+        index = None
+        inp += " " + index
 
-    if not getting_id:
-        try:
-            tweet = tweet[index]
-        except IndexError:
-            return 'error: not that many tweets found'
-
-    if 'retweeted_status' in tweet:
-        rt = tweet["retweeted_status"]
-        rt_text = http.unescape(rt["full_text"]).replace('\n', ' ')
-        text = "RT @%s %s" % (rt["user"]["screen_name"], rt_text)
+    if re.match(r"^#", inp):
+        tweet = get_tweet_by_search_term(api_key, inp, index)
     else:
-        text = http.unescape(tweet["full_text"]).replace('\n', ' ')
-    screen_name = tweet["user"]["screen_name"]
-    time = tweet["created_at"]
+        tweet = get_tweet_by_username(api_key, inp, index)
 
-    time = strftime('%Y-%m-%d %H:%M:%S',
-                    strptime(time, '%a %b %d %H:%M:%S +0000 %Y'))
-
-    return "%s \x02%s\x02: %s" % (time, screen_name, text)
+    return format_tweet(tweet)
 
 
 @hook.api_key('twitter')
